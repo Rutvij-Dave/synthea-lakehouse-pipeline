cb8818466b5297e3fd299b590b5114bb0ce40423
saurav@occept.com <saurav@occept.com>
2026-08-19 06:23:24 +0000

    updates

---
+++ 02_silver/01_claim_fhir_r4.py
@@ -0,0 +1,33 @@
+from pyspark import pipelines as dp
+from pyspark.sql import functions as F
+
+
+SOURCE_TABLE = "claims_lakehouse.bronze.bronze_fhir_r4_raw"
+
+
+@dp.table(
+    name="silver_claim_fhir_r4",
+    comment="Canonical Silver claim resources extracted from Synthea FHIR R4 Bundles."
+)
+def silver_claim_fhir_r4():
+
+    bronze_df = spark.read.table(SOURCE_TABLE)
+
+    return (
+        bronze_df
+        .select(
+            "_ingest_ts",
+            "_record_source",
+            F.explode("entry").alias("entry_item")
+        )
+        .select(
+            "_ingest_ts",
+            "_record_source",
+            F.col("entry_item.resource.resourceType").alias("resource_type"),
+            F.col("entry_item.resource.id").alias("claim_id"),
+            F.col("entry_item.resource").alias("claim_payload")
+        )
+        .filter(
+            F.col("resource_type") == "Claim"
+        )
+    )
